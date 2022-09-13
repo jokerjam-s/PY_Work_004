671b2daa32a8eabc107693d223c2386f999ebce4
Андрей <40392418+jokerjam-s@users.noreply.github.com>
2022-09-13 18:03:20 +0300

    task 1 started

---
+++ Task_001/task_001.py
@@ -0,0 +1,25 @@
+# Вычислить число c заданной точностью d
+#
+# Пример:
+# 
+# - при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
+
+import os
+
+
+# запрос вещественного числа 
+def input_float(msg: str)-> float:
+    return float(input(msg))
+
+# запрос целочисленного числа 
+def input_int(msg: str)-> int:
+    return int(input(msg))
+
+
+## MAIN ##
+
+os.system('cls')
+
+num = input_float('введите число: ')
+precision = input_int('введите точность (чисел после запятой): ')
+
